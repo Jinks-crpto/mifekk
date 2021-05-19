030b5416e0a4c9757e473deff17fb7cc88bdc4d2
Jinks-crpto <77697885+Jinks-crpto@users.noreply.github.com>
2021-05-19 09:47:15 +0530

    Add files via upload

---
+++ C81main.js
@@ -0,0 +1,24 @@
+canvas=document.getElementById("My_canvas")
+ctx=canvas.getContext("2d")
+color="cyan"
+ctx.beginPath();
+ctx.strokeStyle=color;
+ctx.lineWidth=2;
+ctx.arc(200,200,40,0,2*Math.PI);
+ctx.stroke();
+function my_mousedown(e) {
+     mouse_x = e.clientX - canvas.offsetLeft; mouse_y = e.clientY - canvas.offsetTop; console.log("X = " + mouse_x + " ,Y = " + mouse_y); circle(mouse_x , mouse_y);
+     document.getElementById
+     }
+canvas.addEventListener("mousedown", my_mousedown);
+function circle(mouse_x,mouse_y){
+    ctx.beginPath();
+    ctx.strokeStyle=color;
+    ctx.lineWidth=2;
+    ctx.arc(mouse_x,mouse_y,40,0,2*Math.PI);
+    ctx.stroke();
+
+}
+function clearall() { 
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+ }
